c985d95e8ff0d09e83c6b38483a80c8e6caaf3e4
Om-i <115377488+Om-i@users.noreply.github.com>
2024-11-18 01:27:52 +0000

    Initialize project
    using start.spring.io

---
+++ src/main/java/omarspetitions/OmarspetitionsApplication.java
@@ -0,0 +1,13 @@
+package omarspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmarspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmarspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/omarspetitions/OmarspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package omarspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmarspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
